45cdadf4f14d726e63bb56bfdb408f625c9c3bbb
dinochen1020 <dinochencool@gmail.com>
2021-01-20 22:29:14 +0800

    dino

--- mapbox_react/src/photoLibrary/PhotoLibrary.js
+++ mapbox_react/src/photoLibrary/PhotoLibrary.js
@@ -74,112 +74,110 @@ function PhotoLibrary(props) {
         setActiveStep((prevActiveStep) => prevActiveStep - 1);
     };
     return (
-        <body>
-            <div className="intro-container">
-                <div className="intro-wrap">
-                    <form className="intro-form validate-form">
-                        <span className="intro-title">
-                            Department : {users[usersIdx].department}
-                        </span>
-                        <span className="intro-title">
-                            Gender : {users[usersIdx].gender === 0 ? 'Male' : 'Female'}
-                        </span>
-                        <span className="intro-title">
-                            Name : {users[usersIdx].name}
-                        </span>
-                        <span className="intro-title">
-                            Age : {users[usersIdx].age}
-                        </span>
-                        <span className="intro-title">
-                            FB : {users[usersIdx].FB}
-                        </span>
-                        <span className="intro-title">
-                            IG : {users[usersIdx].IG}
-                        </span>
-                        <label className="label-input" for="rating">
-                            Rating if you like it !
-                        </label>
-                        <div className="wrap-input">
-                            <input className="input" placeholder="Eg. (Unlike)1~5(Like)" onChange={(e) => setRating(e.target.value)}></input>
-                            <span className="focus-input"> </span>
-                        </div>
-                        <div className="intro-button">
-                            <button className="send" onClick={() => {
-                                const data = { id: users[usersIdx].id, stars: parseInt(rating) }
-                                giveStars(data)
-                            }}>
-                                <span> SEND </span>
-                            </button>
-                        </div>
-                    </form>
-                    <div className="intro-more flex-col-c-m">
-                        <button className="home" onClick={() => props.switch(1)}>
-                            <span> HOME </span>
+        <div className="intro-container">
+            <div className="intro-wrap">
+                <form className="intro-form validate-form">
+                    <span className="intro-title">
+                        Department : {users[usersIdx].department}
+                    </span>
+                    <span className="intro-title">
+                        Gender : {users[usersIdx].gender === 0 ? 'Male' : 'Female'}
+                    </span>
+                    <span className="intro-title">
+                        Name : {users[usersIdx].name}
+                    </span>
+                    <span className="intro-title">
+                        Age : {users[usersIdx].age}
+                    </span>
+                    <span className="intro-title">
+                        FB : {users[usersIdx].FB}
+                    </span>
+                    <span className="intro-title">
+                        IG : {users[usersIdx].IG}
+                    </span>
+                    <label className="label-input" for="rating">
+                        Rating if you like it !
+                    </label>
+                    <div className="wrap-input">
+                        <input className="input" placeholder="Eg. (Unlike)1~5(Like)" onChange={(e) => setRating(e.target.value)}></input>
+                        <span className="focus-input"> </span>
+                    </div>
+                    <div className="intro-button">
+                        <button className="send" onClick={() => {
+                            const data = { id: users[usersIdx].id, stars: parseInt(rating) }
+                            giveStars(data)
+                        }}>
+                            <span> SEND </span>
                         </button>
-                        <div className="image-viewer">
-                            <div className="display">
-                                <img src={users[usersIdx].photo} />
-                            </div>
-                        </div>                        
-                        <div className="average-rating">
-                            <div className={classes.root}>
-                                <Rating
-                                    name="half-rating-read"
-                                    size="large"
-                                    value={(users[usersIdx].totalVoting
-                                        == 0 ? 0 : users[usersIdx].popularity
-                                        / users[usersIdx].totalVoting).toFixed(1)}
-
-                                    precision={0.1}
-                                    readOnly
-                                />
-                            </div>
-                            <p className="rate-title">Average rating : {users[usersIdx].totalVoting === 0 ? 0 : (users[usersIdx].popularity / users[usersIdx].totalVoting).toFixed(1)}</p>
+                    </div>
+                </form>
+                <div className="intro-more flex-col-c-m">
+                    <button className="home" onClick={() => props.switch(1)}>
+                        <span> HOME </span>
+                    </button>
+                    <div className="image-viewer">
+                        <div className="display">
+                            <img src={users[usersIdx].photo} />
                         </div>
-                        <div className="button_blank">
-                            <MobileStepper
-                              variant="dots"
-                              steps={users.length - 1}
-                              position="static"
-                              activeStep={activeStep}
-                              className={classes2.root}
-                              nextButton={
-                                <Button size="small" onClick={() => {
-                                    if(usersIdx + 1 < users.length)
-                                        setUsersIdx(usersIdx + 1)
-                                    ;handleNext()
+                    </div>                        
+                    <div className="average-rating">
+                        <div className={classes.root}>
+                            <Rating
+                                name="half-rating-read"
+                                size="large"
+                                value={(users[usersIdx].totalVoting
+                                    == 0 ? 0 : users[usersIdx].popularity
+                                    / users[usersIdx].totalVoting).toFixed(1)}
 
-                                }} disabled={usersIdx === users.length - 1} 
-                                className={classes2.button}>
-                                  Next
-                                  {theme.direction === "rtl" ? (
-                                    <KeyboardArrowLeft />
-                                  ) : (
-                                    <KeyboardArrowRight />
-                                  )}
-                                </Button>
-                              }
-                              backButton={
-                                <Button size="small" onClick={() => {
-                                    if (usersIdx - 1 > 0)                                         
-                                        setUsersIdx(usersIdx - 1)
-                                    ;handleBack()                                    
-                                }} disabled={usersIdx === 1}
-                                className={classes2.button}>
-                                  {theme.direction === "rtl" ? (
-                                    <KeyboardArrowRight />
-                                  ) : (
-                                    <KeyboardArrowLeft />
-                                  )}
-                                  Back
-                                </Button>
-                              }
+                                precision={0.1}
+                                readOnly
                             />
                         </div>
+                        <p className="rate-title">Average rating : {users[usersIdx].totalVoting === 0 ? 0 : (users[usersIdx].popularity / users[usersIdx].totalVoting).toFixed(1)}</p>
+                    </div>
+                    <div className="button_blank">
+                        <MobileStepper
+                          variant="dots"
+                          steps={users.length - 1}
+                          position="static"
+                          activeStep={activeStep}
+                          className={classes2.root}
+                          nextButton={
+                            <Button size="small" onClick={() => {
+                                if(usersIdx + 1 < users.length)
+                                    setUsersIdx(usersIdx + 1)
+                                ;handleNext()
+
+                            }} disabled={usersIdx === users.length - 1} 
+                            className={classes2.button}>
+                              Next
+                              {theme.direction === "rtl" ? (
+                                <KeyboardArrowLeft />
+                              ) : (
+                                <KeyboardArrowRight />
+                              )}
+                            </Button>
+                          }
+                          backButton={
+                            <Button size="small" onClick={() => {
+                                if (usersIdx - 1 > 0)                                         
+                                    setUsersIdx(usersIdx - 1)
+                                ;handleBack()                                    
+                            }} disabled={usersIdx === 1}
+                            className={classes2.button}>
+                              {theme.direction === "rtl" ? (
+                                <KeyboardArrowRight />
+                              ) : (
+                                <KeyboardArrowLeft />
+                              )}
+                              Back
+                            </Button>
+                          }
+                        />
                     </div>
                 </div>
             </div>
-        </body>
+        </div>
     )
 }
 
