592f7c3804bd08a9028c86dd81e48251c535d291
novahow <b08902047@ntu.edu.tw>
2021-01-20 17:40:35 +0800

    aaa

--- mapbox_react/src/Intro.js
+++ mapbox_react/src/Intro.js
@@ -1,49 +1,44 @@
 import React, { useEffect, useRef, useState, useCallback } from 'react'
-import { useQuery, useMutation } from "@apollo/client"
-import Deps from './Deps'
-
-const sea_in_deps = {
-    'CSEE': ['CSIE', 'EE'], 'LAW': ['法律'],
-    'INDUSTRY': ['化工', '土木', '機械', '工海', '材料'],
-    'ELITE': ['財金', '會計', '國企', '工管', '資管'],
-    'SOCIAL': ['經濟', '政治', '社工']
-}
-
-const Intro = (props) => {
+import useChat from './useChat'
+import { useQuery, useMutation, useLazyQuery } from '@apollo/client'
+const Deps = (props) => {
     const [exp, setExp] = useState(false)
-    console.log('???', props)
-    const [dp, setDp] = useState(props.value.dep)
-    const back = () => {
-        if (exp) {
-            setExp(false);
-        }
-    }
+
+    const [newUserData, setNewUserData] = useState({})
+    const [users, setUsers] = useState([]);
+    // const [department, setDepartment] = useState('');
 
     return (
         <div>
-            <button className='back' onClick={back}>
-                back
-            </button>
-            {!exp ? (<div className="txtimg">
-                <div className="text">
-                    {dp}
+            <div className='txtimg'>
+                <div className='text'>
+                    {props.ele}
                 </div>
                 <button className={exp ? 'trans' : ''} onClick={() => { exp ? setExp(false) : setExp(true) }}>
                     <i class="fa fa-play fa-2x"></i>
                 </button>
 
-                {/*<img src=
-                    {exp ? ("https://i.imgur.com/nZP0hWp.png")
-                        : ("http://clipart-library.com/images/5cRraerKi.jpg")}
-                    onClick={() => { exp ? setExp(false) : setExp(true) }} />*/}
+            </div>
+            <span>
+                {exp ?
+                    (<span className='bg'>
+                        <div className="left_bg">
+                            <button className='goodbutton' onClick={() => {
+                                props.dep(props.ele);
+                                props.gen(0); props.switch(2)
+                            }}> Boy </button>
+                        </div>
+                        <div className="right_bg">
+                            <button className='goodbutton' onClick={() => {
+                                props.dep(props.ele);
+                                props.gen(1); props.switch(2)
+                            }}> Girl </button>
+                        </div>
+                    </span>
+                    ) : (<div></div>)}
+            </span>
+        </div>
+    )
+}
 
-            </div>) : (sea_in_deps[dp].map(e => (
-                <Deps ele={e} exp={exp}
-                    switch={props.switch} dep={props.dep} gen={props.gen} />
-            )))
-            }
-        </div >
-    );
-};
-
-export default Intro
+export default Deps
